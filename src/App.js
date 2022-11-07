627ced8603e4f459095aad8d8633e8332e78ae2f
HyeonuJeong <hyeonu4945@gmail.com>
2022-11-07 18:09:43 +0900

    useRef 와 변수 비교

--- src/App.js
+++ src/App.js
@@ -3,28 +3,45 @@ import { inc } from 'semver';
 import Timer from './timer';
 
 //ref는 렌더링 되기전까지 저장함
+//let 변수도 렌더링 되기전까지 저장
+//let은 렌더링되면 값이 초기화됨 ref는 언마운트전까지 계속 저장함
 
 function App() {
-  
-  const [count, setCount] = useState(0);
+
+  const [render,setRender]=useState(0)
+
   const countRef=useRef(0)
 
-  const increase =()=>{
-    setCount(count+1)
+  let countVar=0;
+
+  const doRender=()=>{
+    setRender(render+1)
   }
 
-  const increaseref =()=>{
+  const increaseref=()=>{
     countRef.current=countRef.current+1
-    console.log(countRef.current)
+    console.log('ref:',countRef.current)
   }
+
+
+  const increasevar=()=>{
+    countVar=countVar+1
+    console.log('var:',countVar)
+  }
+
+  const print=()=>{
+    console.log(`ref:${countRef.current}, var:${countVar}`)
+  }
+
   // console.log(countRef.current)
   return(
     <div>
-      <p>state:{count}</p>
+      <p>Var:{countVar}</p>
       <p>Ref:{countRef.current}</p>
-      <button onClick={increase}>+</button>
+      <button onClick={doRender}> 렌더링</button>
+      <button onClick={increasevar}>+var</button>
       <button onClick={increaseref}>+ref</button>
-      
+      <button onClick={print}>ref var</button>
     </div>
   )
 }
