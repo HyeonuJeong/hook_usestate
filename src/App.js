2f96f2371e5df44ae4a0ded83099b759f13088c3
HyeonuJeong <hyeonu4945@gmail.com>
2022-11-07 17:59:22 +0900

    useRef

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import { inc } from 'semver';
 import Timer from './timer';
 
+//ref는 렌더링 되기전까지 저장함
 
 function App() {
   
-  const [showTimer, setShowTimer] = useState(true);
+  const [count, setCount] = useState(0);
+  const countRef=useRef(0)
 
+  const increase =()=>{
+    setCount(count+1)
+  }
+
+  const increaseref =()=>{
+    countRef.current=countRef.current+1
+    console.log(countRef.current)
+  }
+  // console.log(countRef.current)
   return(
     <div>
-      {showTimer && <Timer/>}
-      <button onClick={()=> setShowTimer(!showTimer)}>toggle timer</button>
+      <p>state:{count}</p>
+      <p>Ref:{countRef.current}</p>
+      <button onClick={increase}>+</button>
+      <button onClick={increaseref}>+ref</button>
+      
     </div>
   )
 }
